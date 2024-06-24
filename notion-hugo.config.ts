4510b8560e6a005522a059e29e34bdab2440b9a5
Zaid S <59472931+zavelin@users.noreply.github.com>
2024-06-24 14:55:40 +0100

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/ravelin/Notion-DoIt-47b5c53258794a7ca640cadf69c92500?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
